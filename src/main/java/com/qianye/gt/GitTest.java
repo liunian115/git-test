944836af35ac334def33eb219bb3fac4d6173f89
qianye <896207208@qq.com>
2021-11-03 22:09:47 +0800

    * 提交代码

---
+++ src/main/java/com/qianye/gt/GitTest.java
@@ -0,0 +1,11 @@
+package com.qianye.gt;
+
+/**
+ * @author qianye
+ * @create 2021-11-03 22:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我是千夜。");
+    }
+}
